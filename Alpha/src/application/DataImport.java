56470e2f20937d57b97cebbd979fefed816fc9e5
Splitboy <57860416+Splitboy@users.noreply.github.com>
2019-12-17 19:41:21 +0100

    added Data to header

--- Alpha/src/application/DataImport.java
+++ Alpha/src/application/DataImport.java
@@ -10,52 +10,48 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DataImport {
-
     public static void main() {
-
-    Stage primaryStage = new Stage();
-    FileChooser file = new FileChooser();
-    FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
-    file.getExtensionFilters().add(extFilter);
-    file.setTitle("Datei öffnen");
-    File Datei = file.showOpenDialog(primaryStage);
-    String TsvFile = "" + Datei;
-    String FieldDelimiter = "\t";
+        Double[] Data;
+        Stage primaryStage = new Stage();
+        FileChooser file = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        file.getExtensionFilters().add(extFilter);
+        file.setTitle("Datei öffnen");
+        File Datei = file.showOpenDialog(primaryStage);
+        String TsvFile = "" + Datei;
+        String FieldDelimiter = "\t";
         BufferedReader br;
         BufferedReader br2;
-		try {
-        br = new BufferedReader ( new FileReader ( TsvFile ) );
-        long lineAmountCache = br.lines().count() - 1;
-        int lineAmount = (int) lineAmountCache;
-        int currentLine = 0;
-        Double[] Data = new Double[lineAmount];
-        int fLine = 0;
-        String line = null;
-        br2 = new BufferedReader ( new FileReader ( TsvFile ) );
-        while ((line = br2.readLine ( )) != null) {
-            String[] fields = line.split ( FieldDelimiter, -1 );
-            if (fLine == 0) {
-                fLine ++;
+        try {
+            br = new BufferedReader(new FileReader(TsvFile));
+            long lineAmountCache = br.lines().count() - 1;
+            int lineAmount = (int) lineAmountCache;
+            int currentLine = 0;
+            Data = new Double[lineAmount];
+            int fLine = 0;
+            String line = null;
+            br2 = new BufferedReader(new FileReader(TsvFile));
+            while ((line = br2.readLine()) != null) {
+                String[] fields = line.split(FieldDelimiter, -1);
+                if (fLine == 0) {
+                    fLine++;
 
-            }
-            else    {
+                } else {
                     fields[3] = fields[3].replace(",", ".");
                     double DataSolution = Double.parseDouble(fields[3]);
                     Data[currentLine] = DataSolution;
-                    currentLine ++;
+                    currentLine++;
+                }
             }
+            System.out.println(Arrays.asList(Data));
+        } catch (
+                FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        } catch (
+                IOException ex) {
+            Logger.getLogger(Main.class.getName())
+                    .log(Level.SEVERE, null, ex);
         }
-        System.out.println(Arrays.asList(Data));
-    } catch (
-    FileNotFoundException ex) {
-        Logger.getLogger ( Main.class.getName ( ) )
-                .log ( Level.SEVERE, null, ex );
-    } catch (
-    IOException ex) {
-        Logger.getLogger ( Main.class.getName ( ) )
-                .log ( Level.SEVERE, null, ex );
-    }
-
-
     }
 }
